c57000cc23198be3f1eed56cd5397f38381c8554
Chase Harrison <chaseh88@gmail.com>
2022-09-19 20:06:04 -0500

    adding initial alert component & testing setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,30 @@ import Button from '@mui/material/Button';
 
 // Styles
 import './App.css';
+import { Alert } from './components/Alert';
 
 // Hooks
 import { useAlertReducer } from './context/Alert/useAlertReducer';
+
+
+const buttons = [
+  {
+    text: 'Success',
+    alertType: 'success',
+  },
+  {
+    text: 'Error',
+    alertType: 'error',
+  },
+  {
+    text: 'Warning',
+    alertType: 'warning',
+  },
+  {
+    text: 'Info',
+    alertType: 'info',
+  }
+];
 
 
 function App() {
@@ -18,27 +39,21 @@ function App() {
         <h1>
           Alert App
         </h1>
-        <Button
-          variant="contained"
-          onClick={() => showAlert({
-            alertType: "success",
-            text: Math.random().toString(36).substring(2, 9)
-          })}
-        >
-          Show Alert
-        </Button>
+        {buttons.map((button, index) => (
+          <Button
+            key={index}
+            variant="contained"
+            onClick={() => showAlert({
+              alertType: button.alertType as any, // TODO: add type
+              text: Math.random().toString(36).substring(7)
+            })}
+          >
+            {button.text}
+          </Button>
+        ))}
         <div className="alerts">
           {alerts?.length ? (
-            alerts.map((alert) => (
-              <div className="alert" key={alert.id}>
-                <p>
-                  {alert.text}
-                </p>
-                <button onClick={() => hideAlert(alert.id!)}>
-                  X
-                </button>
-              </div>
-            ))
+            alerts.map((alert) => <Alert key={alert.id} {...alert} />)
           ) : (
             <p>
               No alerts
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+
+// Material-UI
+import MuiAlert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+
+// Alert Context
+import { AlertInterface } from '../../context/Alert/context';
+import { useAlertReducer } from '../../context/Alert/useAlertReducer';
+
+export const Alert = (props: AlertInterface) => {
+
+  const { hideAlert } = useAlertReducer();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      hideAlert(props.id!);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [hideAlert, props.id]);
+
+  return (
+    <MuiAlert
+      elevation={6}
+      variant="filled"
+      severity={props.alertType}
+      onClose={() => hideAlert(props.id!)}
+    >
+      <AlertTitle>
+        {props.alertType}
+      </AlertTitle>
+      {props.text}
+    </MuiAlert>
+  )
+}
